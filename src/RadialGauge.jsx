723867ca83ca0f309177e47214ebccffb6804984
PROgienek <mikij333@gmail.com>
2019-11-28 14:45:07 +0100

    check

--- src/RadialGauge.jsx
+++ src/RadialGauge.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import { RadialGauge } from 'nonlinear-canvas-gauges'
+import { RadialGauge } from "nonlinear-canvas-gauges";
 
 class ReactRadialGauge extends React.Component {
   componentDidMount() {
     const options = Object.assign({}, this.props, {
       renderTo: this.el
-    })
-    this.gauge = new RadialGauge(options).draw()
+    });
+    this.gauge = new RadialGauge(options).draw();
   }
 
   componentWillReceiveProps(nextProps) {
-    this.gauge.value = nextProps.value
-    this.gauge.update(nextProps)
+    console.log("check");
+    this.gauge.value = nextProps.value;
+    this.gauge.update(nextProps);
   }
 
   render() {
     return (
-      <canvas ref={(canvas) => {
-        this.el = canvas
-      }} />
-    )
+      <canvas
+        ref={canvas => {
+          this.el = canvas;
+        }}
+      />
+    );
   }
 }
 
-export default ReactRadialGauge
+export default ReactRadialGauge;
