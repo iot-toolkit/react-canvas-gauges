07d3ef29b85d5b5689bbf76da573a66dba042c53
PROgienek <mikij333@gmail.com>
2019-11-28 13:34:11 +0100

    changed dependency from canvas-gauges to nonlinear-canvas-gauges

--- src/LinearGauge.jsx
+++ src/LinearGauge.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-import {LinearGauge} from 'canvas-gauges'
+import { LinearGauge } from 'nonlinear-canvas-gauges'
 
 class ReactLinearGauge extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     const options = Object.assign({}, this.props, {
       renderTo: this.el
     })
     this.gauge = new LinearGauge(options).draw()
   }
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     this.gauge.value = nextProps.value
     this.gauge.update(nextProps)
   }
 
-  render () {
+  render() {
     return (
       <canvas ref={(canvas) => {
         this.el = canvas
--- src/RadialGauge.jsx
+++ src/RadialGauge.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-import {RadialGauge} from 'canvas-gauges'
+import { RadialGauge } from 'nonlinear-canvas-gauges'
 
 class ReactRadialGauge extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     const options = Object.assign({}, this.props, {
       renderTo: this.el
     })
     this.gauge = new RadialGauge(options).draw()
   }
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     this.gauge.value = nextProps.value
     this.gauge.update(nextProps)
   }
 
-  render () {
+  render() {
     return (
       <canvas ref={(canvas) => {
         this.el = canvas
